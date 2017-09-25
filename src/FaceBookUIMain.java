fb6be5fa2a0a5c7fc1e7f1bd02d7e0fb6598d9a5
Bruno Muniz <bruno@softsite.com.br>
2017-09-25 16:33:43 +0900

    Added the main class to make it easier to start the application

---
+++ src/FaceBookUIMain.java
@@ -0,0 +1,11 @@
+
+
+import tc.samples.like.fb.FaceBookUI;
+import totalcross.TotalCrossApplication;
+
+public class FaceBookUIMain {
+	public static void main(String[] args) {
+		TotalCrossApplication.run(FaceBookUI.class, "/scr", "android", "/scale",
+				"1", "/fontsize", "20", "/fingertouch");
+	}
+}
